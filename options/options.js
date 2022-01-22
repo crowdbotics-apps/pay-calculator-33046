68a19173e7ab99dfb09dad632b42dc0bc8a9839f
Crowdbotics <team@crowdbotics.com>
2022-01-22 11:52:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pay_calculator_33046",
+  url: "https://pay_calculator_33046.botics.co",
+  api: "https://pay_calculator_33046.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
